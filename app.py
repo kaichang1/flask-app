11c5443a3b4b00abc8d8106e861d8e543beead0c
KaiHsiang Chang <chankaih@flip2.engr.oregonstate.edu>
2022-02-09 08:58:41 -0800

    draft

--- app.py
+++ app.py
@@ -1,52 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 # Configuration
 
 app = Flask(__name__)
 
-
-
-# Demo
-people_from_app_py = [
-{
-    "name": "Thomas",
-    "age": 33,
-    "location": "New Mexico",
-    "favorite_color": "Blue"
-},
-{
-    "name": "Gregory",
-    "age": 41,
-    "location": "Texas",
-    "favorite_color": "Red"
-},
-{
-    "name": "Vincent",
-    "age": 27,
-    "location": "Ohio",
-    "favorite_color": "Green"
-},
-{
-    "name": "Alexander",
-    "age": 29,
-    "location": "Florida",
-    "favorite_color": "Orange"
-}
-]
-
-
-
 # Routes 
 
 @app.route('/')
-def root():
-    return render_template("main.j2", people=people_from_app_py)
+def index():
+    return render_template('index.html')
+
+@app.route('/customers', methods=['POST', 'GET'])
+def customers():
+    return render_template('customers.html')
+
+@app.route('/orders', methods=['POST', 'GET'])
+def orders():
+    return render_template('orders.html')
+
+@app.route('/order-details', methods=['POST', 'GET'])
+def order_details():
+    return render_template('order-details.html')
+
+# @app.route('/orders')
+# def root():
+#     return render_template("main.j2", people=people_from_app_py)
+
+# @app.route('/order_details')
+# def root():
+#     return render_template("main.j2", people=people_from_app_py)
 
 # Listener
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 9112)) 
+    port = int(os.environ.get('PORT', 9111)) 
     #                                 ^^^^
     #              You can replace this number with any valid port
     
